6b8956efc52e1379ef9bc06ffd5be4a596d8da67
harrymmurphy <143562527+harrymmurphy@users.noreply.github.com>
2023-10-17 00:46:39 -0500

    Update code.py

--- code.py
+++ code.py
@@ -20,8 +20,30 @@ timeseries = data.drop(['MONTH', 'YEAR'], axis = 1)
 sns.lineplot(data=timeseries.replace('nan', float('nan')).melt(id_vars=['DATE']),
              x='DATE', y= "value", hue='variable')
 
-# Hodrick-Prescott Filtering
+### Hodrick-Prescott Filtering
 
+# Lambda Parameter calculation 100*(period)^2
 lambda_param = 100*(12)^2  
-trend, cyclical = sm.tsa.filters.hpfilter(timeseries['Wheat'], lamb=lambda_param)
 
+
+timeseries1 = timeseries.iloc[:,0:25]
+
+# Hodrick-Prescott Filtering
+def Hodrick_Prescott (self):
+    for col in cols:
+        trend, cyclical = sm.tsa.filters.hpfilter(timeseries[col], lamb=lambda_param)
+        print(cyclical.head(500)) #observe values, make sure they make sense
+        print(trend.head(500)) # same as above^
+        return(trend)
+        return(cyclical)
+HP_df = pd.DataFrame()
+cols = timeseries1.columns
+HP_df[cols] = timeseries1[cols].apply(Hodrick_Prescott)
+print(HP_df.head(100))
+
+# Verifty that apply function 
+trend,cyclical = sm.tsa.filters.hpfilter(timeseries['Beef'], lamb=lambda_param)
+print(trend.head(100))
+
+date = timeseries['DATE']
+HP_timeseries = pd.concat([HP_df, date], axis=1).reindex(HP_df.index)
